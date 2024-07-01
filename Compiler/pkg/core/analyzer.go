a93086131f6c93b30b19b996250337479127eb01
Robert Monden <robert@robertmonden.com>
2024-07-01 20:41:04 +0200

    Added some code to analyzer.go

--- Compiler/pkg/core/analyzer.go
+++ Compiler/pkg/core/analyzer.go
@@ -1,10 +1,42 @@
 package core
 
+type VariableScope struct {
+	childScopes []VariableScope
+	variables   map[string]Variable
+	parentScope *VariableScope
+	depth       int8
+}
+
+func (s VariableScope) InScope(name string) bool {
+	if _, found := s.variables[name]; found {
+		return true
+	}
+
+	if s.parentScope == nil {
+		return false
+	}
+
+	return s.parentScope.InScope(name)
+}
+
+type Variable struct {
+	Name        string
+	Type        string
+	IsPointer   bool
+	SizeInBytes uint8
+}
+
 type Analyzer struct {
 	Ast AstTreeNode
 }
 
 func InitializeAnalyzer(tree AstTreeNode) (*Analyzer, error) {
+	// rootScope := VariableScope{
+	// 	childScopes: []VariableScope{},
+	// 	parentScope: nil,
+	// 	depth:       0,
+	// }
+
 	return &Analyzer{
 		Ast: tree,
 	}, nil
